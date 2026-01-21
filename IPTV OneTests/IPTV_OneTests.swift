4af7013eea4782b5f85ca1ea2dda947941950a0c
Robin Nap <Robin.nap@icloud.com>
2026-01-21 20:42:29 +0100

    Initial Commit

---
+++ IPTV OneTests/IPTV_OneTests.swift
@@ -0,0 +1,16 @@
+//
+//  IPTV_OneTests.swift
+//  IPTV OneTests
+//
+//  Created by Robin Nap on 21/01/2026.
+//
+
+import Testing
+
+struct IPTV_OneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
